a568a341955b4b340cd988bbc7690b9db6aaffc7
ramondejesusmaciasramirez <ramondejesusmaciasramirez@gmail.com>
2024-07-15 18:52:10 +0000

    -->Estilos agregados --!Falta info --!Falta busqueda global !!Tablas y migraciones sin acceso aun

--- resources/views/Categorias/Switches.blade.php
+++ resources/views/Categorias/Switches.blade.php
@@ -60,7 +60,7 @@
                 </tr>
             </thead>
             <tbody>
-                 @foreach ($switches as $switches)
+                @foreach ($switches as $switches)
                     <tr data-id="{{ $switches->id }}">
                         <td>{{ $switches->NOMBRE_PC }}</td>
                         <td>{{ $switches->No_SERIE }}</td>
@@ -70,6 +70,76 @@
                     </tr>
                 @endforeach
             </tbody>
-        </table>
+        </table><br>
+
+        <button type="button" onclick="tableToCSV()">
+            Export as CSV
+        </button><br>
     </div>
+
+
+    {{-- Logica para exportar mi busqueda actual a CSV --}}
+    <script type="text/javascript">
+        function tableToCSV() {
+
+            // Variable to store the final csv data
+            let csv_data = [];
+
+            // Get each row data
+            let rows = document.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+
+                // Get each column data
+                let cols = rows[i].querySelectorAll('td,th');
+
+                // Stores each csv row data
+                let csvrow = [];
+                for (let j = 0; j < cols.length; j++) {
+
+                    // Get the text data of each cell
+                    // of a row and push it to csvrow
+                    csvrow.push(cols[j].innerHTML);
+                }
+
+                // Combine each column value with comma
+                csv_data.push(csvrow.join(","));
+            }
+
+            // Combine each row data with new line character
+            csv_data = csv_data.join('\n');
+
+            // Call this function to download csv file
+            downloadCSVFile(csv_data);
+
+        }
+
+        function downloadCSVFile(csv_data) {
+
+            // Create CSV file object and feed
+            // our csv_data into it
+            CSVFile = new Blob([csv_data], {
+                type: "text/csv"
+            });
+
+            // Create to temporary link to initiate
+            // download process
+            let temp_link = document.createElement('a');
+
+            // Download csv file
+            temp_link.download = "Consulta.csv";
+            let url = window.URL.createObjectURL(CSVFile);
+            temp_link.href = url;
+
+            // This link should not be displayed
+            temp_link.style.display = "none";
+            document.body.appendChild(temp_link);
+
+            // Automatically click the link to
+            // trigger download
+            temp_link.click();
+            document.body.removeChild(temp_link);
+        }
+    </script>
+
+
 @endsection
