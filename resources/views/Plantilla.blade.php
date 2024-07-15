31bf1d31bdab761a42a610a3d7910ef53e9993dc
ramondejesusmaciasramirez <ramondejesusmaciasramirez@gmail.com>
2024-07-15 23:19:59 +0000

    ->Estilos time

--- resources/views/Plantilla.blade.php
+++ resources/views/Plantilla.blade.php
@@ -30,12 +30,12 @@
     <header>
     <nav>
     <div class="left-links">
-        <a href="{{ asset('Index') }}"><img src="{{ asset('/Imagenes/LogoColor.png') }}" alt="SEMRC-IT Data Automatization"></a>
+        <a href="{{ asset('Index') }}"><img src="{{ asset('/Imagenes/Logo.png') }}" alt="SEMRC-IT Data Automatization"></a>
         <a href="#">Inventario</a>
     </div>
     <div class="right-links">
-        <input type="search" placeholder="Search" aria-label="Search">
-        <button type="submit">Buscar</button>
+        <input type="search" placeholder="Search" aria-label="Search" id="GlobalSearch">
+        <button type="submit" id="GlobalSearchButton">Buscar</button>
         <a href="#">Usuario</a>
         <a href="{{ asset('/') }}">Salir</a>
     </div>
